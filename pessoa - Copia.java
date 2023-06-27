183643c94be62473afdbb7ac4b78b6d5216b21bb
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-06-27 18:30:32 -0300

    Add files via upload

---
+++ pessoa - Copia.java
@@ -0,0 +1,17 @@
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+
+    public Pessoa(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+}
