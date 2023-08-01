fe1f84f18a243587b16362a3f5c862b0f90433fa
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-08-01 09:38:29 -0300

    Update and rename MenuCadastroPessoas.java to cadastro.java

---
+++ cadastro.java
@@ -0,0 +1,26 @@
+package cadastro;
+
+public class Cadastro {
+	
+	private String nome;
+	private String sobrenome;
+	
+	public String getListar() {
+		String dados = this.nome + " " + this.sobrenome;
+		return String.format(dados, this.nome, this.sobrenome);
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+}
